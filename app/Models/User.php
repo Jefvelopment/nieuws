cb0a448b20f71ae8fcc4b3427d8b1bd52011a135
Jeffrey van Zon <191317@novacollege.nl>
2025-09-08 15:32:55 +0200

    Two-sided function made

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function posts()
+    {
+        return $belongsToMany(Post::class, 'user_posts');
 }
