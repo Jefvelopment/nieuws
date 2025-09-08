75cfab83b3713bd78c288fef5ce814e3e78f111e
Jeffrey van Zon <191317@novacollege.nl>
2025-09-08 15:33:09 +0200

    Seeders filled

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -12,7 +13,14 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-      $categories = ['Sports', 'Politics', 'Technology', 'Health', 'Entertainment', 'Business', 'Science', 'World'];
-    }
+
+$categories = [
+            ['name' => 'Technology', 'description' => 'Posts related to technology.'],
+            ['name' => 'Health', 'description' => 'Posts related to health and wellness.'],
+            ['name' => 'Travel', 'description' => 'Posts about travel experiences and tips.'],
+            ['name' => 'Food', 'description' => 'Posts about food and recipes.'],
+            
+        ];
         Category::insert($categories);
+    }
 }
