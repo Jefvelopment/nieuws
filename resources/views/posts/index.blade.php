4a6de8ad4ee85bf4d49d85de96b940f80a2562f6
Jeffrey van Zon <191317@novacollege.nl>
2025-09-29 15:08:21 +0200

    base layout remade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,16 +1,16 @@
-<x-base-layout>
+<x-layout>
 @foreach($posts as $post)
 <article>
     <h1>
-        <a href="{{ route('posts.show', $post->id) }}" class="text-blue-600 hover:underline">
+        <a href="{{ route('posts.show', $post->id) }}">
             {{$post->title}}
         </a>
     </h1>
     <p>{{$post->content}}</p>
     <div>
-    {{$post->category->title}}
-</div>
+        {{$post->category->title}}
+    </div>
 </article>
 <button formaction="{{ route('posts.edit', $post) }}" formmethod="GET">Edit</button>
 @endforeach
-</x-base-layout>
+</x-layout>
--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -1,12 +1,14 @@
 <x-base-layout>
     <h1 class="text-3xl font-bold mb-6">Posts with tag: {{ $tag->name }}</h1>
     <ul>
-        @foreach($tag->posts as $post)
+        @forelse($tag->posts as $post)
             <li>
                 <a href="{{ route('posts.show', $post->id) }}" class="text-blue-600 hover:underline">
                     {{ $post->title }}
                 </a>
             </li>
-        @endforeach
+        @empty
+            <li>No posts found for this tag.</li>
+        @endforelse 
     </ul>
 </x-base-layout>
